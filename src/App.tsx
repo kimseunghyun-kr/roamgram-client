cf338ef875d15ec436e4b67ef102833dac021b66
Alexandra <shirokumochan@gmail.com>
2024-06-01 20:55:26 +0800

    added marker for default location using googles map api

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import GoogleMapWrapper from './components/google/GoogleMapWrapper.tsx';
-import Header from './components/Header/Header.tsx'
-import Home from './components/Home/Home.tsx';
-import Schedule from './components/Schedule/Schedule.tsx';
-import Place from './components/Place/Place.tsx';
+import GoogleMapWrapper from "./components/google/GoogleMapWrapper.tsx";
+import Header from "./components/Header/Header.tsx";
+import Home from "./components/Home/Home.tsx";
+import Schedule from "./components/Schedule/Schedule.tsx";
+import Place from "./components/Place/Place.tsx";
 
 function App() {
-
   return (
     <>
-    <Header></Header>
-    <Schedule></Schedule>
-
+      <Header></Header>
+      <Schedule></Schedule>
     </>
   );
 }
 
-export default App
+export default App;
--- src/components/Schedule/Schedule.tsx
+++ src/components/Schedule/Schedule.tsx
@@ -5,43 +5,41 @@ import { SiGo, SiGooglemaps } from "react-icons/si";
 
 function Schedule() {
   return (
-    <main>
-      <div className="main-map">
-        <form className="form-location">
-          <div className="scheduler-schedule">
-            <div className="scheduler-edit">
-              <label className="start-logo" htmlFor="from">
-                <SiGooglemaps></SiGooglemaps>
-                <input
-                  className="f form-for"
-                  id="from"
-                  type="text"
-                  placeholder="Search Start Location Here"
-                ></input>
-              </label>
-              <label className="end-logo" htmlFor="to">
-                <SiGooglemaps></SiGooglemaps>
-                <input
-                  className="f form-to"
-                  id="to"
-                  type="text"
-                  placeholder="Search End-Location here"
-                ></input>
-              </label>
-              <br></br>
-              <button className="btn btn-location">Search</button>
-            </div>
+    <div className="main-map">
+      <form className="form-location">
+        <div className="scheduler-schedule">
+          <div className="scheduler-edit">
+            <label className="start-logo" htmlFor="from">
+              <SiGooglemaps></SiGooglemaps>
+              <input
+                className="f form-for"
+                id="from"
+                type="text"
+                placeholder="Search Start Location Here"
+              ></input>
+            </label>
+            <label className="end-logo" htmlFor="to">
+              <SiGooglemaps></SiGooglemaps>
+              <input
+                className="f form-to"
+                id="to"
+                type="text"
+                placeholder="Search End-Location here"
+              ></input>
+            </label>
+            <br></br>
+            <button className="btn btn-location">Search</button>
           </div>
-        </form>
-        <div className="container-fluid">
-          <div className="google-map" id="googleMap">
-            <h1>Map</h1>
-            <GoogleMapWrapper></GoogleMapWrapper>
-          </div>
-          <div id="output">testing here</div>
         </div>
+      </form>
+      <div className="container-fluid">
+        <div className="google-map" id="googleMap">
+          <GoogleMapWrapper></GoogleMapWrapper>
+          <h1>Map</h1>
+        </div>
+        <div id="output">testing here</div>
       </div>
-    </main>
+    </div>
   );
 }
 
