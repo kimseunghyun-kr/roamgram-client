187813266f3085b9f065f3e61a9a71bf7a884a8f
kimseunghyun <incongnito12@gmail.com>
2024-06-06 22:09:54 +0900

    committing new version of MapPage

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -3,26 +3,69 @@ import React from "react";
 import { useJsApiLoader } from "@react-google-maps/api";
 import GoogleMaps from "../components/GoogleMaps/GoogleMaps.tsx";
 import {
-  MantineProvider,
-  Center,
-  Loader,
-  Flex,
-  Container,
-  Group,
-  Button,
-  Image,
-  Switch,
-  NativeSelect,
+    MantineProvider,
+    Center,
+    Grid,
+    Loader,
+    Flex,
+    Container,
+    Group,
+    Button,
+    Image,
+    Switch,
+    NativeSelect,
 } from "@mantine/core";
 
 const MapPage = () => {
-  const { isLoaded } = useJsApiLoader({
-    googleMapsApiKey: import.meta.env.VITE_APP_GOOGLE_KEY,
-    libraries: ["places", "maps", "core", "marker", "routes"],
-    version: "weekly",
-  });
+    const { isLoaded } = useJsApiLoader({
+        googleMapsApiKey: import.meta.env.VITE_APP_GOOGLE_KEY,
+        libraries: ["places", "maps", "core", "marker", "routes"],
+        version: "weekly",
+    });
 
-  return isLoaded ? <GoogleMaps></GoogleMaps> : <></>;
+    return isLoaded ? (
+        <MantineProvider>
+          <Flex>
+            <Flex>
+              <Image src="src\assets\RoamGram Logo.png" h={45} w="auto"></Image>
+            </Flex>
+            <Container>
+              <Group gap="xs" grow>
+                <div className="header-button">
+                  <Button variant="transparent" size="xl">
+                    Book
+                  </Button>
+                  <Button variant="transparent" size="xl">
+                    Planner
+                  </Button>
+                  <Button variant="transparent" size="xl">
+                    Route
+                  </Button>
+                </div>
+              </Group>
+            </Container>
+            <Center>
+              <Group gap="xs">
+                <Button>Login</Button>
+                <Button>Register</Button>
+                <Switch>Mode</Switch>
+                <NativeSelect
+                  radius={1}
+                  data={["ENG", "CHI", "JPN", "KR"]}
+                  styles={{
+                    input: { color: "gray", border: "none", textAlign: "left" },
+                  }}
+                ></NativeSelect>
+              </Group>
+            </Center>
+          </Flex>
+          <Grid>
+            <GoogleMaps></GoogleMaps>
+          </Grid>
+        </MantineProvider>
+      ) : (
+        <></>
+      );
 };
 
 export default MapPage;
