21d048a615f91347ff23acbc696a99ce60085f16
Alexandra <shirokumochan@gmail.com>
2024-06-06 18:44:50 +0800

    with some ui changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,20 @@ import { useJsApiLoader } from "@react-google-maps/api";
 import "./App.css";
 import GoogleMaps from "./components/GoogleMaps/GoogleMaps.tsx";
 import { Loader } from "@googlemaps/js-api-loader";
+import {
+  MantineProvider,
+  AppShell,
+  Container,
+  Image,
+  Group,
+  Button,
+  Flex,
+  Switch,
+  NativeSelect,
+  Grid,
+  Center,
+} from "@mantine/core";
+import "@mantine/core/styles.css";
 
 function App() {
   const { isLoaded } = useJsApiLoader({
@@ -9,7 +23,47 @@ function App() {
     libraries: ["places", "maps", "core", "marker", "routes"],
     version: "weekly",
   });
-  return isLoaded ? <GoogleMaps></GoogleMaps> : <></>;
+  return (
+    <MantineProvider>
+      <Flex>
+        <Flex>
+          <Image src="src\assets\RoamGram Logo.png" h={45} w="auto"></Image>
+        </Flex>
+        <Container>
+          <Group gap="xs" grow>
+            <div className="header-button">
+              <Button variant="transparent" size="xl">
+                Book
+              </Button>
+              <Button variant="transparent" size="xl">
+                Planner
+              </Button>
+              <Button variant="transparent" size="xl">
+                Route
+              </Button>
+            </div>
+          </Group>
+        </Container>
+        <Center>
+          <Group gap="xs">
+            <Button>Login</Button>
+            <Button>Register</Button>
+            <Switch>Mode</Switch>
+            <NativeSelect
+              radius={1}
+              data={["ENG", "CHI", "JPN", "KR"]}
+              styles={{
+                input: { color: "gray", border: "none", textAlign: "left" },
+              }}
+            ></NativeSelect>
+          </Group>
+        </Center>
+      </Flex>
+      <Grid>
+        <GoogleMaps></GoogleMaps>
+      </Grid>
+    </MantineProvider>
+  );
 }
-
+// {isLoaded ? <GoogleMaps></GoogleMaps> : <></>}
 export default App;
