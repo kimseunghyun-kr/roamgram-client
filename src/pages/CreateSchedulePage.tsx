441de407c03c10d4953ee87bd676dbd1fa18106d
kimseunghyun <incongnito12@gmail.com>
2024-06-09 02:13:20 +0900

    mid-progress report

--- src/pages/CreateSchedulePage.tsx
+++ src/pages/CreateSchedulePage.tsx
@@ -1,18 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { ScheduleInsertRequest } from '../types/request/ScheduleInsertRequest';
-import { useAddSchedule } from '../hooks';
+import { useAddSchedule, useFetchTravelPlan } from '../hooks';
 import { Place } from '../types/Place';
 import { format } from 'date-fns';
 import { Link } from "react-router-dom";
-
 const CreateSchedulePage: React.FC = () => {
   const { id } = useParams<{ id: string }>();
   const today = new Date()
+  // Fetch travel plan and handle loading state within the custom hook
+  const { travelPlan, refetch } = useFetchTravelPlan(id!);
+  console.log("travel Plan at {} ", travelPlan)
+  const fetchedDate = new Date(travelPlan?.travelStartDate!)
+  const formattedDate = malformedDateCorrector(fetchedDate);
+  console.log(formattedDate)
   const [travelDate, setTravelDate] = useState<string>(format(today, 'yyyy-MM-dd'));
   const [travelStart, setTravelStart] = useState<string>('');
   const [travelEnd, setTravelEnd] = useState<string>('');
-
   const [newSchedule, setNewSchedule] = useState<ScheduleInsertRequest>({
     place: null,
     previousScheduleId: '',
@@ -23,6 +27,10 @@ const CreateSchedulePage: React.FC = () => {
   const [place, setPlace] = useState<Place | null>(null);
   const addSchedule = useAddSchedule();
   const navigate = useNavigate();
+
+  useEffect(() => {
+    refetch(); // Refetch data on location change
+  }, [location, refetch]);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -83,6 +91,20 @@ const CreateSchedulePage: React.FC = () => {
       <button type="submit" className="btn btn-primary">Add Schedule</button>
     </form>
   );
+
+  function malformedDateCorrector(malformedDate : Date) {
+    const [year, month, day] = malformedDate.toLocaleDateString().split('.')
+    console.log(year, month, day)
+  // Pad the month and day with leading zeros if needed
+  // const paddedMonth = month.length === 1 ? '0' + month : month;
+  // const paddedDay = day.length === 1 ? '0' + day : day;
+
+  // Construct the new date string in 'yyyy-MM-dd' format
+  // const newDateString = `${year}-${paddedMonth}-${paddedDay}`;
+  // console.log(newDateString)
+
+  return malformedDate;
+  }
 };
 
 export default CreateSchedulePage;
