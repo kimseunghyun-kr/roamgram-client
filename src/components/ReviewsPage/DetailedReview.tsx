e48186ebbcc060d6c1dfcac421513a414fd28e65
Alexandra <shirokumochan@gmail.com>
2024-07-06 22:38:06 +0800

    starting reviews to backend

--- src/components/ReviewsPage/DetailedReview.tsx
+++ src/components/ReviewsPage/DetailedReview.tsx
@@ -1,7 +1,7 @@
 import ReactQuill, { Quill } from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import Header from "../Header/Header";
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import {
   Button,
   Card,
@@ -20,6 +20,142 @@ export function DetailedReview() {
 
   //fonts
   const Font = Quill.import("formats/font");
+
+  const formats = [
+    "size",
+    "font",
+    "color",
+    "background",
+    "align",
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+  ];
+
+  const quilRef = useRef(null);
+  const [ratingValue, setRatingValue] = useState(null);
+
+  async function cloudinaryImageUpload(file) {
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("upload_preset", import.meta.env.VITE_CLOUDINARY_PRESET);
+
+    const res = await fetch(
+      `https://api.cloudinary.com/v1_1/${
+        import.meta.env.VITE_CLOUDINARY_NAME
+      }/image/upload`,
+      {
+        method: "POST",
+        body: formData,
+      }
+    ).then((res) => res.json());
+    const url = res.url;
+
+    console.log(res);
+
+    return url;
+  }
+
+  const authToken = sessionStorage.getItem(`authToken`);
+
+  const getPresignedURL = useCallback(async (s3Body) => {
+    const res = await fetch(`https://localhost/media-file/upload-file-small`, {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${authToken}`,
+      },
+      body: JSON.stringify(s3Body),
+    }).then((res) => res.json());
+    return res;
+  }, []);
+
+  const uploadAmazonS3 = async (file, s3Body) => {
+    const presigned_url = await getPresignedURL(s3Body);
+    console.log("presign", presigned_url);
+
+    const formData = new FormData();
+    formData.append("upload", file);
+    console.log(`${presigned_url}`);
+    console.log(file.type);
+    const res = await fetch(`${presigned_url}`, {
+      method: "PUT",
+      body: file,
+      headers: {
+        "Content-Type": file.type,
+      },
+    });
+
+    console.log(res);
+  };
+
+  const getFroms3 = async (objectKey) => {
+    const get_url = fetch(`https://localhost/media-file/get-file`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(objectKey),
+    }).then((res) => res.json());
+
+    return get_url; //this is the s3 url we will get
+  };
+
+  const scheduleId = "3eb1f5e8-ed0b-49ec-b709-662f0ed104c6"; //we will append this to our url when creating review instead
+
+  //async update so we shall not use this!
+  const [fileBody, setFileBody] = useState({
+    scheduleId: scheduleId,
+    fileSize: 0,
+    originalFileName: "",
+    contentLocation: 0,
+  });
+  ///////////////////////////
+
+  const imageHandler = useCallback(() => {
+    const quilEditor = quilRef.current.getEditor();
+    const quilSelection = quilEditor.getSelection(); //gets the current location users curser is at
+    const imageInput = document.createElement("input"); //image input
+    imageInput.setAttribute("type", "file"); //file types only
+    imageInput.setAttribute("accept", "image/*"); //accepts images
+    imageInput.click(); //mimicks click
+    console.log(imageInput);
+    console.log(quilSelection, "quillSelection");
+    //if we have a file uploaded to reactQuill editor
+
+    const reader = new FileReader();
+    imageInput.onchange = async () => {
+      if (imageInput !== null && imageInput.files !== null) {
+        console.log(imageInput.files);
+        const file = imageInput.files[0];
+
+        const fileSize = file.size;
+        const fileName = file.name;
+        const contentLocation = quilSelection.index;
+        console.log("file is", file);
+        const s3Body = {
+          scheduleId: scheduleId,
+          fileSize: fileSize,
+          originalFileName: fileName,
+          contentLocation: contentLocation,
+        };
+        //getPresignedURL(s3Body);
+        uploadAmazonS3(file, s3Body);
+        //const url = cloudinaryImageUpload(file);
+        //const imgPlaceHolder = "https://placehold.co/600x400?text=Placeholder";
+        //quilEditor.insertEmbed(quilSelection, "image", imgPlaceHolder);
+      }
+    };
+  }, []);
 
   const modules = useMemo(
     () => ({
@@ -47,42 +183,6 @@ export function DetailedReview() {
     }),
     []
   );
-
-  const formats = [
-    "size",
-    "font",
-    "color",
-    "background",
-    "align",
-    "header",
-    "bold",
-    "italic",
-    "underline",
-    "strike",
-    "blockquote",
-    "list",
-    "bullet",
-    "indent",
-    "link",
-    "image",
-  ];
-
-  const quilRef = useRef(null);
-  const [ratingValue, setRatingValue] = useState(null);
-
-  function imageHandler() {
-    const quilEditor = quilRef.current.getEditor();
-    const quilSelection = quilEditor.getSelection(); //gets the current location users curser is at
-    console.log(quilEditor);
-    console.log(quilSelection);
-    const imageInput = document.createElement("input"); //image input
-    imageInput.setAttribute("type", "file");
-    imageInput.setAttribute("accept", "image/*");
-
-    imageInput.click();
-    //const imgPlaceHolder = "https://placehold.co/600x400?text=Placeholder";
-    //quilEditor.insertEmbed(quilSelection, "image", imgPlaceHolder);
-  }
 
   return (
     <>
