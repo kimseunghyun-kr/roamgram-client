a68292b475e9bda6a2063e84f8ffe137f473df8b
Alexandra <shirokumochan@gmail.com>
2024-07-29 14:56:17 +0800

    text editor your reviews use react quill instead

--- src/components/ReviewsPage/ReviewsHTML.tsx
+++ src/components/ReviewsPage/ReviewsHTML.tsx
@@ -1,10 +1,25 @@
 import { useLocation } from "react-router-dom";
 import Header from "../Header/Header";
-import { Container, Paper, Text, Title } from "@mantine/core";
+import { Container, Paper, Space, Text, Title } from "@mantine/core";
 import "./ReviewsHTML.css";
+import { useEffect, useRef } from "react";
+import ReactQuill from "react-quill";
 
 function ReviewsHTML(props) {
   const pageHTML = useLocation()?.state["pageHTML"];
+  const modules = {
+    toolbar: false, // Disable the toolbar in read-only mode
+  };
+  console.log(pageHTML);
+  const quilRef = useRef(null);
+
+  useEffect(() => {
+    if (quilRef.current) {
+      const editor = quilRef.current.getEditor();
+      editor.clipboard.dangerouslyPasteHTML(pageHTML);
+    }
+  }, [pageHTML]);
+
   console.log(pageHTML);
   return (
     <>
@@ -15,14 +30,22 @@ function ReviewsHTML(props) {
         <div>
           <Container mt={40}>
             <Title ff="Roboto">Review Details</Title>
-            <Paper withBorder shadow="md" p={30} mt={15} radius="md">
+            {/* <Paper withBorder shadow="md" p={30} mt={15} radius="md">
               <Text
                 className="quill-content"
                 dangerouslySetInnerHTML={{
                   __html: pageHTML,
                 }}
-              />
-            </Paper>
+              /> */}
+            <Space h={20} />
+            <ReactQuill
+              ref={quilRef}
+              value={""}
+              readOnly={true}
+              theme="snow"
+              modules={modules}
+            />
+            {/* </Paper> */}
           </Container>
         </div>
       </body>
